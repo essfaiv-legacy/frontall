dd8637f9e021082c0dd736d1c3c08a46e79a6e3e
Gilberto Tavares <camaleaun@gmail.com>
2018-04-15 01:51:03 -0300

    WPF Hello

---
+++ HelloWpf/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Interop;
+
+namespace HelloWpf
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            IconHelper.RemoveIcon(this);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Foo1_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            setButtonVisibility();
+        }
+
+        private void Foo2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            setButtonVisibility();
+        }
+
+        private void setButtonVisibility()
+        {
+            if ((Foo1.Text != String.Empty) && (Foo2.Password != String.Empty))
+            {
+                Foo4.IsEnabled = true;
+            }
+            else
+            {
+                Foo4.IsEnabled = false;
+            }
+        }
+    }
+
+    public static class IconHelper
+    {
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hwnd, IntPtr hwndInsertAfter, int x,
+    int y, int width, int height, uint flags);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr
+    lParam);
+
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_DLGMODALFRAME = 0x0001;
+        const int SWP_NOSIZE = 0x0001;
+        const int SWP_NOMOVE = 0x0002;
+        const int SWP_NOZORDER = 0x0004;
+        const int SWP_FRAMECHANGED = 0x0020;
+        const uint WM_SETICON = 0x0080;
+
+        public static void RemoveIcon(Window window)
+        {
+            // Get this window's handle
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            // Change the extended window style to not show a window icon
+            int extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_DLGMODALFRAME);
+            // Update the window's non-client area to reflect the changes
+            SetWindowPos(hwnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE |
+    SWP_NOZORDER | SWP_FRAMECHANGED);
+        }
+    }
+}
